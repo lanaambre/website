d30b66cb849b7b3b901b659168e11c85ea073400
Lana-Ambre Demailly <lanaambre.demailly@gmail.com>
2022-06-17 14:44:12 +0200

    Update favicon

--- pages/_document.js
+++ pages/_document.js
@@ -9,6 +9,26 @@ export default function Document() {
           rel="stylesheet"
         />
         <meta name="robots" content="index,follow,all,noodp"></meta>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#9b9a0b" />
+        <meta name="msapplication-TileColor" content="#00aba9" />
+        <meta name="theme-color" content="#ffffff"></meta>
       </Head>
       <body>
         <Main />
